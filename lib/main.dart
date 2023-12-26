8812d46929dc9d925afbbc203df4698d0f56b95b
JanibekJali <janibekjali@gmail.com>
2023-12-26 11:47:56 +0600

    Tapshyrma 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+// Muslim - + funksiyasyn jaz
+// Nurjamal eje - - funksiyasyn
+// Javlon - * funksiyasyn jaz 
+// Adam kalkulatordoi sandy jazganda koshu, kemitip, koboitup bersin 
+  // Named Function
+  // Unamed function
+  // Function With parameter
+  // Call Back Function
+
+kosh(int san1, int san2){
+final san = san1 + san2;
+print(san);
+}
+}
